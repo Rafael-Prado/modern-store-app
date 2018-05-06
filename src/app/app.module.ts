f57c4f71c8c239723d42f5720c04ba7381bf4498
Rafael da cruz do prado <rafaeldacruzdoprado@hotmail.com>
2018-05-06 18:16:23 -0300

    Adiconando componetes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeadbarComponent } from './Componets/shared/headbar/headbar.component';
+import { SubMenuComponent } from './Componets/shared/sub-menu/sub-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadbarComponent,
+    SubMenuComponent
   ],
   imports: [
     BrowserModule
